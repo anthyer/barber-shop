48b9048e442e5bd436f30e3b6190a9a1c7fa98b8
Antonio Marcos da Silva <111596082+anthyer@users.noreply.github.com>
2024-05-02 09:04:45 -0300

    aula-08-01-02

--- src/Model/Servico.java
+++ src/Model/Servico.java
@@ -41,4 +41,14 @@ public class Servico
     {
         this.valor = valor;
     }
+    
+    public int getId()
+    {
+        return id;
+    }
+    
+    public void setId(int id)
+    {
+        this.id = id;
+    }
 }
